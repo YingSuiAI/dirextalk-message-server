2577958e96c7542b6253c427b50f0d820a1e11a1
adam <849607831@qq.com>
2026-07-08 23:52:34 +0800

    refactor: point storage aliases at owning packages

--- p2p/storage/aliases.go
+++ p2p/storage/aliases.go
@@ -1,41 +1,45 @@
 package storage
 
-import "github.com/YingSuiAI/dirextalk-message-server/p2p/domain"
+import (
+	"github.com/YingSuiAI/dirextalk-message-server/internal/dirextalkdomain"
+	"github.com/YingSuiAI/dirextalk-message-server/internal/dirextalkplugin"
+	"github.com/YingSuiAI/dirextalk-message-server/p2p/domain"
+)
 
-type portalState = domain.PortalState
-type readMarker = domain.ReadMarker
-type channel = domain.Channel
-type channelInviteGrant = domain.ChannelInviteGrant
+type portalState = dirextalkdomain.PortalState
+type readMarker = dirextalkdomain.ReadMarker
+type channel = dirextalkdomain.Channel
+type channelInviteGrant = dirextalkdomain.ChannelInviteGrant
 type channelPostRecord = domain.ChannelPostRecord
 type channelCommentRecord = domain.ChannelCommentRecord
 type contactRecord = domain.ContactRecord
 type groupRecord = domain.GroupRecord
-type callRecord = domain.CallRecord
-type favoriteRecord = domain.FavoriteRecord
-type followRecord = domain.FollowRecord
-type reactionRecord = domain.ReactionRecord
-type memberRecord = domain.MemberRecord
-type p2pEvent = domain.Event
-type eventBounds = domain.EventBounds
-type blockRecord = domain.BlockRecord
-type reportRecord = domain.ReportRecord
-type pluginCatalogEntry = domain.PluginCatalogEntry
-type pluginInstance = domain.PluginInstance
-type pluginJob = domain.PluginJob
-type pluginSecret = domain.PluginSecret
-type conversationKind = domain.ConversationKind
-type conversationRecord = domain.ConversationRecord
+type callRecord = dirextalkdomain.CallRecord
+type favoriteRecord = dirextalkdomain.FavoriteRecord
+type followRecord = dirextalkdomain.FollowRecord
+type reactionRecord = dirextalkdomain.ReactionRecord
+type memberRecord = dirextalkdomain.MemberRecord
+type p2pEvent = dirextalkdomain.Event
+type eventBounds = dirextalkdomain.EventBounds
+type blockRecord = dirextalkdomain.BlockRecord
+type reportRecord = dirextalkdomain.ReportRecord
+type pluginCatalogEntry = dirextalkplugin.CatalogEntry
+type pluginInstance = dirextalkplugin.Instance
+type pluginJob = dirextalkplugin.Job
+type pluginSecret = dirextalkplugin.Secret
+type conversationKind = dirextalkdomain.ConversationKind
+type conversationRecord = dirextalkdomain.ConversationRecord
 
 const (
-	conversationKindDirect       = domain.ConversationKindDirect
-	conversationKindGroup        = domain.ConversationKindGroup
-	conversationKindChannel      = domain.ConversationKindChannel
-	conversationKindSystem       = domain.ConversationKindSystem
-	conversationLifecycleDeleted = domain.ConversationLifecycleDeleted
+	conversationKindDirect       = dirextalkdomain.ConversationKindDirect
+	conversationKindGroup        = dirextalkdomain.ConversationKindGroup
+	conversationKindChannel      = dirextalkdomain.ConversationKindChannel
+	conversationKindSystem       = dirextalkdomain.ConversationKindSystem
+	conversationLifecycleDeleted = dirextalkdomain.ConversationLifecycleDeleted
 )
 
 func normalizeConversationRecord(record conversationRecord) conversationRecord {
-	return domain.NormalizeConversationRecord(record)
+	return dirextalkdomain.NormalizeConversationRecord(record)
 }
 
 func conversationFromContact(contact contactRecord) conversationRecord {
@@ -47,13 +51,13 @@ func conversationFromGroup(group groupRecord) conversationRecord {
 }
 
 func conversationFromChannel(ch channel) conversationRecord {
-	return domain.ConversationFromChannel(ch)
+	return dirextalkdomain.ConversationFromChannel(ch)
 }
 
 func contactDeleted(status string) bool {
-	return domain.ContactDeleted(status)
+	return dirextalkdomain.ContactDeleted(status)
 }
 
 func fallbackString(value, fallback string) string {
-	return domain.FallbackString(value, fallback)
+	return dirextalkdomain.FallbackString(value, fallback)
 }
